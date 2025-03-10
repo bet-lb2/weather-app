822ae5dbb2b484edee6cfb0e6b53bfb436ffbd88
shota <55336324+bet-lb2@users.noreply.github.com>
2025-03-10 12:31:29 +0900

    implement some function and variables

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import "./styles.css";
 
 const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 const apiKey = "ZP6DTL8KGEGSPQW5GRSSZL5QW";
-const location = document.getElementById("location");
+const cityName = document.getElementById("city-name");
+const searchBtn = document.getElementById("search-btn");
 const output = document.getElementById("output");
 
 async function fetchWeatherData(location) {
@@ -14,28 +15,25 @@ async function fetchWeatherData(location) {
     throw new Error(response.status);
 }
 
-function fahrenheitToCelsius(temperature) {
-
+function extractNecessaryData(data) {
+    return { address: data.address, temperature: fahrenheitToCelsius(data.currentConditions.temp), feelsLike: fahrenheitToCelsius(data.currentConditions.feelslike), humidity: data.currentConditions.humidity, windSpeed: data.currentConditions.windspeed , resolvedAddress: data.resolvedAddress };
 }
 
-function celsiusToFahrenheit(temperature) {
-
+function fahrenheitToCelsius(temp) {
+    const celsius = (temp - 32) / 1.8;
+    return celsius.toFixed(2);
 }
 
-window.addEventListener("load", async () => {
-    const locationName = location.value;
-    const result = await fetchWeatherData(locationName);
-    console.log(result)
-    const { address, currentConditions, days, description, latitude, longitude, resolvedAddress, timezone} = result;
-    const { conditions, datetime, feelslike, humidity, pressure, sunrise, sunset, temp, uvindex, windgust, windspeed } = currentConditions;
-    console.log(address, conditions, datetime, feelslike, humidity, pressure)
+searchBtn.addEventListener("click", async () => {
+    const inputValue = cityName.value;
+    const data = await fetchWeatherData(inputValue);
+    const extractedData = extractNecessaryData(data);
+    output.style.visibility = "visible";
     output.innerHTML = "";
     output.innerHTML = `
-    <div id="location-name">Location Name: ${resolvedAddress}</div>
-    <div id="temperature">Temperature: ${temp}°(Fahrenheit)</div>
-    <div id="condition">Condition: ${conditions}</div>
-    <div id="humidity">Humidity: ${humidity}%</div>
-    <div id="feelslike">Feels like :${feelslike}</div>
-    <div id="pressure">Pressure: ${pressure}hpa</div>
-    `
+    <div class="resolvedAddress">${extractedData.resolvedAddress}</div>
+    <div class="temperature">${extractedData.temperature} °C</div>
+    <div class="feels-like">Feels like: ${extractedData.feelsLike} °C</div>
+    <div class="humidity">Humidity: ${extractedData.humidity} %</div>
+    <div class="wind-speed">Wind Speed: ${extractedData.windSpeed} km/h</div>`
 })
